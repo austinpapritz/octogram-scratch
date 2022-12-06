07384c03e50017fc806b857d8a0b3d8f09b4fdaf
Austin Papritz <austin@papritz.dev>
2022-12-05 16:18:37 -0800

    stars are desploying on profile, need upstar downstar funcitonality

--- profiles/profiles.js
+++ profiles/profiles.js
@@ -3,6 +3,8 @@ import { getProfileById } from '../fetch-utils.js';
 const avatarImg = document.querySelector('#avatar-image');
 const usernameHeader = document.querySelector('.username-h2');
 const profileDetail = document.querySelector('.profile-detail');
+const starsDiv = document.querySelector('.stars-div');
+
 const params = new URLSearchParams(location.search);
 const id = params.get('id');
 
@@ -17,4 +19,35 @@ async function displayProfile() {
     usernameHeader.textContent = profile.data.username;
     avatarImg.src = profile.data.avatar_url;
     profileDetail.textContent = profile.data.bio;
+
+    const profileStars = renderStars(profile.data);
+    starsDiv.append(profileStars);
+}
+
+function renderStars({ stars, username, id }) {
+    const p = document.createElement('p');
+    const downButton = document.createElement('button');
+    const upButton = document.createElement('button');
+
+    const profileStars = document.createElement('div');
+
+    profileStars.classList.add('profile-stars');
+    profileStars.append(p, upButton, downButton);
+
+    downButton.textContent = 'downvote user ⬇️';
+    upButton.textContent = 'upvote user ⬆️';
+    p.classList.add('profile-name');
+
+    p.textContent = `${username} has ${stars} ⭐`;
+
+    // downButton.addEventListener('click', async () => {
+    //     await decrementStars(id);
+    //     await fetchAndDisplayProfile();
+    // });
+    // upButton.addEventListener('click', async () => {
+    //     await incrementStars(id);
+    //     await fetchAndDisplayProfile();
+    // });
+
+    return profileStars;
 }
